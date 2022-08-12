fbe332415642b24b944929e1d75fab485b596c5d
namitos <namit@yandex.ru>
2022-08-12 18:51:46 +0300

    schema refactor

--- schema/schema.go
+++ schema/schema.go
@@ -14,6 +14,21 @@ const (
 
 var complexTypeNames = Enum{TypeNameMap, TypeNameObject, TypeNameArray}
 
+var primitiveNumberKinds = []reflect.Kind{
+	reflect.Int,
+	reflect.Int8,
+	reflect.Int16,
+	reflect.Int32,
+	reflect.Int64,
+	reflect.Uint,
+	reflect.Uint8,
+	reflect.Uint16,
+	reflect.Uint32,
+	reflect.Uint64,
+	reflect.Float32,
+	reflect.Float64,
+}
+
 type Schema struct {
 	Type           string             `json:"type,omitempty"`
 	TypeName       string             `json:"typeName,omitempty"`
@@ -31,21 +46,6 @@ type WidgetSettings map[string]interface{}
 func Get(t reflect.Type) *Schema {
 	schema, _ := fillValue(t, map[string]string{}, nil)
 	return schema
-}
-
-var primitiveNumberKinds = []reflect.Kind{
-	reflect.Int,
-	reflect.Int8,
-	reflect.Int16,
-	reflect.Int32,
-	reflect.Int64,
-	reflect.Uint,
-	reflect.Uint8,
-	reflect.Uint16,
-	reflect.Uint32,
-	reflect.Uint64,
-	reflect.Float32,
-	reflect.Float64,
 }
 
 func getKindPrimitiveType(kind reflect.Kind) string {
